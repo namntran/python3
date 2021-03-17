6e1e3447b7a9c486bd78ae5def608ab247dd4614
namntran <namtran78@gmail.com>
2021-03-17 16:51:46 +1000

    program to estimate volume of concrete required

---
+++ 02/concrete.py
@@ -0,0 +1,7 @@
+# program to calculate volume of concrete required
+
+length = float(input("Length of park (m): "))
+width = float(input("Width of park (m): "))
+litres = float(input("Litres per square metre: "))
+
+print("Litres require = ", length * width * litres)
