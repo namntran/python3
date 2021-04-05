6631760a037357d31fb86aa37730dd29ff74079b
namntran <namtran78@gmail.com>
2021-04-05 17:14:00 +1000

    program to print fibonacci sequence with 4 elements per row

--- 04/fibonacci.py
+++ 04/fibonacci.py
@@ -1,11 +1,11 @@
 # Fibonacci sequence is the sum of the two preceding ones e.g. 0 1 1 2 3 5 6
-
 list1 = [] # create an empty list to store numbers
 # variables
 x = 0
 y = 1
 count = 0
 length = int(input("Enter a positive number: "))
+n_elements = 4
 # generate the Fibonacci sequence
 while x < length:
 	# print(x, end = "\n") # temp
@@ -15,7 +15,19 @@ while x < length:
 	x = y
 	y = z
 	count += 1
-#use map function to convert items in list to a string and then join use space ' ' as separator
-print (' '.join(map(str, list1)))
+# use map function to convert items in list to a string and then join use space ' ' as separator
+# print (' '.join(map(str, list1))) # temporary prints all elements horizontally
 
+# for i, e in enumerate(list1, n_elements):
+#     print(e) #temporary prints vertically one element per row
 
+# partition list into sub-lists using matrices with a maximum width of 4
+matrix = [list1[i:i+4] for i in range(0,len(list1),4)]
+for l in matrix:
+		# use map function to convert items in list to a string and then join use space ' ' as separator
+		print (' '.join(map(str, l)))
+"""
+import numpy as np
+a = np.array([list1])
+print a.reshape((3,4))
+"""
