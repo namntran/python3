e944a49a44a700e889f0082034ffe086a4f8c3ce
namntran <namtran78@gmail.com>
2021-04-29 21:34:46 +1000

    calculate highest average marks for students and courses

---
+++ 06/problem4.py
@@ -0,0 +1,58 @@
+# problem4.py
+# program that allows the user to enter the marks of 5 students in 4 courses
+# output the highest average marks for students and courses
+
+def get_average(list):
+    """ function to get average score """
+    total = 0
+    for i in list:
+        total += i
+    return total/len(list)
+
+# declare variables
+grades = []# list to store grades
+students = 5 # rows
+courses = 4 # columns
+# lists to store grades of different courses
+course1 = []
+course2 = []
+course3 = []
+course4 = []
+# list to store the average of student marks
+highestAverageStudentMarks = []
+
+for i in range(students):
+    # ask for user input
+    print("Student",i + 1,"(courses 1 - 4): ", end="")
+    # separate list of integers from user input and separate
+    row = list(map(int, input().split(" ")))
+    # add the integers to a list
+    grades.append(row)
+
+for students in grades:
+    # get the average of the list
+    average = get_average(students)
+    # add the average of each student into list
+    highestAverageStudentMarks.append(average)
+# printing the highest average student marks from list
+print('The highest average mark of students:', max(highestAverageStudentMarks))
+
+# add grades into different courses (columns)
+for courses in grades:
+    course1.append(courses[0])
+    course2.append(courses[1])
+    course3.append(courses[2])
+    course4.append(courses[3])
+
+# calculate the average for each course
+averageCourse1 = get_average(course1)
+averageCourse2 = get_average(course2)
+averageCourse3 = get_average(course3)
+averageCourse4 = get_average(course4)
+# add the average of each course to a list
+highestAverageCourseMarks = [averageCourse1, averageCourse2, averageCourse3, averageCourse4]
+
+# print(course1, course2, course3, course4) # temporary value for debugging
+# print(averageCourse1, averageCourse2, averageCourse3, averageCourse4) # temporary value for debugging
+# printing the highest average course marks from list
+print('The highest average mark of courses: ', max(highestAverageCourseMarks))
