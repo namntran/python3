8ecd94583d7fb82cf48bdcd589f906a1e9f6654f
namntran <namtran78@gmail.com>
2021-05-11 17:58:28 +1000

    program to rotate the first and last numbers iteratively

--- 08/problem1.py
+++ 08/problem1.py
@@ -1,38 +1,26 @@
-	# Get a list use input
-	# Get a string, split and get a list of integers
-	# Print original list first
-	# Each iteration, remove first element use push. Pop then get 2, 3,4.
-	# For i in list, pop index [0]
-	# Have a variable for popped number to receive popped element
-	# Append popped element at end of list
-	# Initial form, length of list = 4 iterations. If length of list = 4, then iterations is 4 loops
-#Write a function that converts all digits in a string to underline.
-# def rotateList (numbers):
-#     """Convert all numbers in a string to underscore."""
-#     # for i in sentence:
-#     #     #check if character in user input is a number using isdigit() method, boolean
-#     #     if i.isdigit() == True:
-#     #         #replace the numbers in string to underline using string replace() method
-#     #         sentence = sentence.replace(i, "_")
-#     return numbers
+# program to rotate the numbers so the first number becomes the last
+# the rest numbers move one position forward
 
-#ask user to enter String input
-# sentence = input("Input a string: ")
+def rotateNumber():
+	"""function to rotate the first and last numbers iteratively """
+	# ask for user input
+	list1 = str(input("Input a list: "))
 
+	# split string into separate characters
+	list1 = list1.split()
+	# if length of input list = 4, then iterations is 4 loops
+	length = len(list1)
 
-
-list1 = str(input("Input a list: "))
-
-# split string into separate characters
-list1 = list1.split()
-length = len(list1)
-
-# convert strings in list to integers using map() function
-list1 = list(map(int, list1))
-print(list1)
-# print(len(list1))
-
-# print(list1)
-for i in range(length):
-	list1.append(list1.pop(0))
+	# convert strings in list to integers using map() function
+	list1 = list(map(int, list1))
+	# print original list first
 	print(list1)
+
+	# each iteration, remove first element by pop() method then repeat for element 2, 3, 4.
+	for i in range(length):
+		# append popped element at end of list
+		list1.append(list1.pop(0))
+		print(list1)
+
+# call the function
+rotateNumber()
