309a15bcc0f3dfdda990479cd42e1a6c2bd7c481
namntran <namtran78@gmail.com>
2021-05-13 16:57:13 +1000

    program to merge two lists in descending order

--- 08/problem4.py
+++ 08/problem4.py
@@ -37,7 +37,7 @@
 
 # Answer b)
 
-def merge(list1, list2):
+def mergeLists(list1, list2):
     list3 = []
     length1 = len(list1)
     length2 = len(list2)
@@ -53,17 +53,29 @@ def merge(list1, list2):
             i += 1
     list3 = list3 + list2[j:] + list1[i:]
     return list3
-	# if not list1:
-    # 	return list2[::-1]
-    # if not list2:
-    # 	return list1[::-1]
-    # if list1[-1] < list2[-1]:
-    #     return [list2[-1]] + merge(list1, list2[:-1])
-    # else:
-    #     return [list1[-1]] + merge(list1[:-1], list2)
 
-l1 = [1,3,3,6]
-l2 = [1,4,5]
+# l1 = [1,3,3,6]
+# l2 = [1,4,5]
+# use while loop to end input blank line
+while True:
+	# ask user for input string
+	list1 = str(input("List 1: "))
+	list2 = str(input("List 2: "))
+	# check if sentence empty, if so break out of loop
+	if len(list1) or len(list2) == 0:
+		break
+# split string into separate characters
+list1 = list1.split()
+list2 = list2.split()
 
-print(merge(l1,l2))
+# convert strings in list to integers using map() function
+list1 = list(map(int, list1))
+list2 = list(map(int, list2))
+# call function to merge two lists in ascending order
+merged_list = mergeLists(list1,list2)
+# sort merged list in descending order
+merged_list.sort(reverse=True)
+# print out of merged list
+print(merged_list)
+# print(merge(l1,l2))
 
