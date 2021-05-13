2fc05bec4950811ee47c62cbbc48caa3ca059978
namntran <namtran78@gmail.com>
2021-05-13 20:31:07 +1000

    program to merge two lists in descending order

--- 08/problem4.py
+++ 08/problem4.py
@@ -1,61 +1,29 @@
-	# Two lists in random order
-	# Allowed to have duplicate lists
-	# Need a third list
-	# You can use append and sort function to sort lists respectively
-	# When to update i, when to update j
+# program to merge two lists in descending order
 
-	# L1 = [2,4,13,6,3]
-	# L2 = [23,4,5,1,7]
-
-	# Sort L1 in descending order
-	# L1 =[13, 6, 4, 3, 2
-	# L2 = [23, 7, 5, 4, 1]
-	# L3 = [23, 13, 7, ],
-	# i = 0
-	# j = 0
-	# 			j += 1
-	# 			i + = 1
-	# 	If L1[i] > L2[i]
-	# 		appendL2
-	# I = 0 0 1 1 2 2
-	# J = 0 1 1 2 2 3
-	# While loop:
-	# 	When reach end off
-
-	# l,j = 0
-	# Check L,J
-	# 	Check L1[0] and L2[0] for bigger
-
-# def merge(list1, list2):
-#     res = list1 + list2
-#     res.sort()
-#     return res
-
-# l1 = [1,2,3]
-# l2 = [4,5,6,7]
-# print(merge(l1,l2))
-
-# Answer b)
-
+"""Function to merge two lists"""
 def mergeLists(list1, list2):
+	# third list to store variables
     list3 = []
     length1 = len(list1)
     length2 = len(list2)
     i, j = 0, 0
-	# If L1[i] > L2[i]
-	# 		appendL2
+
     while i < length1 and j < length2:
+		# Check list1[0] and list2[0] for bigger
         if list1[i] > list2[j]:
+			# append number to list
             list3.append(list2[j])
+			# increment count
             j += 1
         else:
+			# append number to list
             list3.append(list1[i])
+			# increment count
             i += 1
+	# merge lists and return output
     list3 = list3 + list2[j:] + list1[i:]
     return list3
 
-# l1 = [1,3,3,6]
-# l2 = [1,4,5]
 # use while loop to end input blank line
 while True:
 	# ask user for input string
@@ -64,6 +32,7 @@ while True:
 	# check if sentence empty, if so break out of loop
 	if len(list1) or len(list2) == 0:
 		break
+
 # split string into separate characters
 list1 = list1.split()
 list2 = list2.split()
@@ -71,11 +40,11 @@ list2 = list2.split()
 # convert strings in list to integers using map() function
 list1 = list(map(int, list1))
 list2 = list(map(int, list2))
-# call function to merge two lists in ascending order
+
+# call function to merge two lists from user input
 merged_list = mergeLists(list1,list2)
 # sort merged list in descending order
 merged_list.sort(reverse=True)
-# print out of merged list
-print(merged_list)
-# print(merge(l1,l2))
 
+# print merged list
+print(merged_list)
