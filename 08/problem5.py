8fd3977f6632d6e848d409cf1a6e95cbd933c66e
namntran <namtran78@gmail.com>
2021-05-14 11:41:00 +1000

    program to find ground zero patient and new patients infected at each meeting

--- 08/problem5.py
+++ 08/problem5.py
@@ -1,33 +1,55 @@
-	# Each new meeting, you get one new patient infected
+# program to find ground zero patient and new patients infected at each meeting
 
-	# #at loop 6
-	# 	For loop
-	# 		List_of_string
+"""function to find if meeting attendees infected """
+def isCovid(rows):
+    # permanent list to record infections
+    infectedList = []
+    # for loop to traverse file line by line
+    for line in rows:
+		# temporary list to record new infections
+        newInfected = []
+        attendees = line.split()
+		# variables needed to record Harry's index in meeting 6
+        meetingNumber = attendees[0]
+        meetings = attendees[1:]
+        index = 0
 
-	# 		For loop for traversing the name of the list_of_string:
+        for name in meetings:
+            # check if the name endswith() method with * for fever (Harry)
+            if name.endswith("*"):
+                newInfected.append(name.strip("*"))
+				# if this person is infected add to temporary list
+                newInfected.append(meetings[index-1])
+				# check for meeting attendees
+                if index == (len(meetings) -1):
+                    index = -1
+				# append new names to list
+                newInfected.append(meetings[index+1])
+                # break the loop
+                break
 
-	# 			If name endswith"*"
+            for infected in infectedList:
+                if name == infected:
+					# if this person is infected add to temporary list
+                    newInfected.append(meetings[index-1])
+					# check meeting number
+                    if index == (len(meetings) -1):
+                        index = -1
+					# append new names to list
+                    newInfected.append(meetings[index+1])
+            index+=1
+		# add temporary list to permanent list
+        infectedList = list(set(infectedList + newInfected))
 
-	# 				Add the name in the list
-
-	# 			# find neighbours and append to infected list [i +1] [i -1]
-
-	# 			Name = check index of name to see if it in infected list, append to infected list
-
-	# Open and read file
-	# For loop, use split function to get list of strings
-	# Need for loop to traverse file line by line (get meeting number and attendees)
-	# 	→ Condition, check last character for asterisk at each name
-	# 	→ Put Harry in infected_list[]
-	# At loop 6, you have a list of strings, need another variable to record Harry's index
-
-def getInfected(output):
-	"""function to find common elements """
+        if len(infectedList):
+            # Print meeting number and names of infected
+            print(meetingNumber, " ".join(infectedList), len(infectedList))
 
 # ask user for file name
 path = input("File name: ")
 # list to store all the lines to get list of strings
 list1 = []
+covidList = []
 count = 0
 
 # open and read file from user input
@@ -40,12 +62,15 @@ for name in rows:
     # append lines to list
     list1.append([str(names) for names in name.split()])
 
-# for loop to traverse line by line and print output
-for name in list1:
-    # print each row
-    print(rows[count])
-    # increment count
-    count += 1
+# for loop to traverse line by line and print output - temporary for debugging
+# for name in list1:
+#     # print each row
+#     print(rows[count])
+#     # increment count
+#     count += 1
+
+# function is called here
+isCovid(rows)
 
 # close file
 file.close()
