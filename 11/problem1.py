aa125153a06043de00aee3f71050e43a7b6249f8
namntran <namtran78@gmail.com>
2021-06-02 15:20:52 +1000

    problem 1 solved

--- 11/problem1.py
+++ 11/problem1.py
@@ -2,42 +2,59 @@ class DNS:
     # constructor
     def __init__(self):
         self.dns = dict()
-        # self.blocklist = []
 
-    # domain to IPA
+    # add domain to IP address
     def add_DomainIPA(self, domain, ipa):
         self.dns[domain] = ipa
 
-    # If no record, return None
-    def find_DomainIPA(self, domain):
-        return self.dns.get(domain, None)
+    # If no record found, return None
+    def find_Domain(self, domain):
+        if domain not in self.dns:
+            return None
+        return self.dns.get(domain)
 
+# help menu to list available commands for user
 def helpMenu():
     print("Available commands are 'find', 'add', 'quit',")
 
+# welcome message
 print('Welcome to DNS Server')
 print('Type help for available commands')
-
+# variable to store new DNS record
 dns = DNS()
 
 # use sentinel pattern
 while True:
+    # ask user for input
     userInput = input('$$$ ')
-
-    if userInput == 'help':
+    # call helpMenu() function to print available commands
+    if userInput.lower() == 'help':
         helpMenu()
 
-    elif userInput == 'find':
-        d = input('Enter domain: ')
-        if dns.find_DomainIPA(d) == None:
-            print('None')
-        else:
-            print('IPA ' + dns.find_DomainIPA(d))
+    # find DNS record
+    elif userInput.lower() == 'find':
+        # ask for user input
+        domain = input('Enter domain: ')
+        # no record found
+        if dns.find_Domain(domain) == None:
+            print('No record found')
+        else: # if record found, print domain name
+            print('IP Address ' + dns.find_Domain(domain))
 
-    elif userInput == 'add':
-        d = input('Enter domain: ')
-        ipa = input('Enter IPA: ')
-        dns.add_DomainIPA(d, ipa)
+    # add DNS record
+    elif userInput.lower() == 'add':
+        # ask for user input
+        domain = input('Enter domain: ')
+        # ask for user input
+        ipa = input('Enter IP Address: ')
+        # add domain name and IP address record from user input
+        dns.add_DomainIPA(domain, ipa)
 
+    # quit and exit program
+    elif userInput.lower() == 'q' or 'quit':
+        print("Goodbye")
+        break
+
+    # catch all other user inputs
     else:
         print('Bad command')
